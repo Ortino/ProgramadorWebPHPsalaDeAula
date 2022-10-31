a802442d790bf267574b0a467efd0eeefe77d4b3
Ortino <alcantara-felipe@hotmail.com>
2022-10-31 19:46:06 -0300

    atualizado
    atialuzado

--- banco_de_dados/conexao.php
+++ banco_de_dados/conexao.php
@@ -9,5 +9,5 @@ $conexao = new mysqli($hostname, $usuario, $senha, $banco_de_dados);
 if($conexao->connect_errno){ // error + number = errno
     echo "Falha ao conectar: (" . $conexao->connect_errno .")" . $conexao->connect_error;
 } else {
-    // echo "Conectado ao Banco.<br>";
+    echo "Conectado ao Banco.<br>";
 }
