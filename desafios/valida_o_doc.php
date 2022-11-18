3243cdbf44675def75ab802637b5056bed7dceaf
Marcos Felipe Alcantara Vieira <111320612+Ortino@users.noreply.github.com>
2022-11-17 23:14:10 -0300

    atualizado
    atualizado

--- desafios/valida_o_doc.php
+++ desafios/valida_o_doc.php
@@ -1,3 +1,35 @@
+<?php
+
+    if(isset($_POST['br_enviar'])){
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $sexo = $_POST['sexo'];
+        $idade = $_POST['idade'];
+        $telefone = $_POST['telefone'];
+        $peso = $_POST['peso'];
+
+        // array de erros
+        $erros =[];
+
+        // santilizar= limpezar dos dados
+        $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);   //specaial chars = os botões especiais não vai aparecer     
+        $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL );
+        $sexo = filter_input(INPUT_POST, 'sexo', FILTER_SANITIZE_ADD_SLASHES);             
+        $idade = filter_input(INPUT_POST, 'idade', FILTER_SANITIZE_NUMBER_INT);           
+        $telefone = filter_input(INPUT_POST, 'telefone', FILTER_SANITIZE_NUMBER_INT);        
+        $peso = filter_input(INPUT_POST, 'peso', FILTER_SANITIZE_NUMBER_INT, FILTER_FLAG_ALLOW_FRACTION); //2 permitir fração
+        
+        echo "<p>Nome : $nome </p>";
+        echo "<p>E - mail : $email </p>";
+        echo "<p>Sexo : $sexo </p>";
+        echo "<p>idade : $idade </p>";
+        echo "<p>telefone : $telefone </p>";
+        echo "<p>peso : $peso </p>";
+               
+    }
+    
+
+?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
@@ -16,9 +48,12 @@
             <input type="email" name="email">
         </label><br>
         <label> 
-            Sexo
-            <input type="sexo" name="sexo">
-        </label><br>
+        <label>Sexo</label>
+				<select class="form-select" id="estadoc" name="estado_civil" required>
+					<option selected disabled value="">Selecione</option>
+					<option value="Masculuno">Masculino</option>
+					<option value="Feminino">Feminino</option>
+					</select><br>
         <label>
             Idade
             <input type="number" min="1" max="120" step="1"  name="idade"> anos 
@@ -27,7 +62,7 @@
         <label>
             Telefone
             <input type="number" name="tel">
-        </label>
+        </label><br>
             Peso
             <input type="number" min="2.4"  max="200.0" step="0.1" name="peso"> Kilos
         </label><br>
