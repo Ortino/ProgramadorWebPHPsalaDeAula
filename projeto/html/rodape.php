033bd178e71426538dc0c55c1efa4f08d1615bdd
eduardolinsmotta <eduardolinsmotta@gmail,com>
2023-01-16 21:52:45 -0300

    atualizado
    atualizado

--- projeto/html/rodape.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    <section id="rodaPe">
-        &copy; Site Estudo
-        <a href="#" style="float: right; line-height: 50px;" >
-            <img data-bs-toggle="modal" data-bs-target="#funcionario_modal" src="img/restrito.png" alt="Área restrita" title="Área restrita">
-        </a>
-    </section>
-</footer>
--- projeto/protect.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(!isset($_SESSION)){
-    session_start();
-}
-
-if(!isset($_SESSION['id'])){
-    header("Location: nao_permitidp.php");
-}
-?>
